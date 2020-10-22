b565a9e027eadfe7c84dc5c5b5c235197c81dd2b
samuhay <samuhay@hotmail.com>
2020-10-22 13:45:40 +0300

    Paper update

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {Provider as PaperProvider} from 'react-native-paper';
 import {ThemeProvider} from 'styled-components';
 import HomeScreen from './src/Home';
 import LoginScreen from './src/Login';
@@ -33,25 +34,27 @@ const theme = {
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            cardStyle: theme.colors.notWhite,
-          }}>
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{headerShown: false}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ThemeProvider>
+    <PaperProvider>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              cardStyle: theme.colors.notWhite,
+            }}>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{headerShown: false}}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
+    </PaperProvider>
   );
 };
 
--- src/Login.js
+++ src/Login.js
@@ -7,12 +7,16 @@
 //Main React import
 import React from 'react';
 //Our Styles for Project
+import {TextInput} from 'react-native-paper';
 import {Form, Body} from './styles/wrapper';
 import {RoundedButton} from './styles/buttons';
 import {SubTitle, Title} from './styles/text';
 import {FinalInput} from './styles/input';
 
-const LoginScreen = ({navigation}) => {
+const LoginScreen = ({navigation}, props) => {
+  const [textE, setEmail] = React.useState('');
+  const [textP, setPassword] = React.useState('');
+
   return (
     <Body>
       <Title>Stay in touch</Title>
@@ -29,14 +33,18 @@ const LoginScreen = ({navigation}) => {
           textContentType="emailAddress"
           /*inlineImageLeft='search_icon'*/
         />
-        <FinalInput
-          textColor="#000"
-          bottom="15"
-          color="white"
-          autoCompleteType="email"
-          keyboardType="email-address"
-          textContentType="emailAddress"
-          /*inlineImageLeft='search_icon'*/
+        <TextInput
+          label="Email"
+          value={textE}
+          onChangeText={(text) => setEmail(text)}
+          mode="outlined"
+          style={{color: colors.dark}}
+        />
+        <TextInput
+          label="Password"
+          value={textP}
+          onChangeText={(text) => setPassword(text)}
+          mode="outlined"
         />
         <RoundedButton
           color="#2d9cdb"
@@ -47,7 +55,7 @@ const LoginScreen = ({navigation}) => {
         <RoundedButton
           margin="10px"
           color="#ffffff"
-          onPress={''} //TODO: We will add register here
+          onPress={'#'} //TODO: We will add register here
           title="Sing In"
           textColor="#000000"
         />
