3fc70c10bb2cf49cde5fe680ea585fbb28b9ee2e
samuhay <samuhay@hotmail.com>
2020-11-10 21:54:25 +0300

    Text Input Changes

--- src/styles/wrapper.js
+++ src/styles/wrapper.js
@@ -15,7 +15,6 @@ export const Line = styled.View`
   flex-direction: row;
   justify-content: ${(props) => props.justify || 'space-around'};
   alignItems: center;
-  paddingVertical: 20px;
 `;
 export const Start = styled.View`
   flex: 1;
@@ -24,7 +23,7 @@ export const Start = styled.View`
   padding: ${(props) => props.paddings || '0 37px'};
 `;
 export const Bottom = styled.View`
-  flex:1;
+  flex: 1;
   width: 100%;
   position: absolute;
   bottom: 20px;
